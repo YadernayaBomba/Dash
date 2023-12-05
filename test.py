bea94c37d15bd680e46d695aa28a54866d90e64c
glebc <glebchikgrub@gmail.com>
2023-12-06 03:01:41 +1000

    1.1

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+# data.py веб-приложение
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+import dash_table
 
+# Считываем данные из CSV-файла
+file_path = 'C:/Users/glebc/OneDrive/Рабочий стол/1.csv'
+df = pd.read_csv(file_path)
